9f88033b7547ec59dcac820bb8fabac6e4930128
Brian Check <b9check@gmail.com>
2025-01-05 01:48:35 -0500

    updated site

--- personalsite/main/views.py
+++ personalsite/main/views.py
@@ -2,17 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def hello_world(request):
-    return HttpResponse("Hello, World!")
-
+def resume(request):
+    return render(request, 'resume.html')
 
 def home(request):
     return render(request, 'home.html')
 
-
 def projects(request):
     return render(request, 'projects.html')
-
 
 def contact(request):
     return render(request, 'contact.html')
--- personalsite/personalsite/settings.py
+++ personalsite/personalsite/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "main" / "static",  # your source static folder
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"  # a separate, distinct folder
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
