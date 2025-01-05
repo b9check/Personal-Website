bc9ecfb30df9fe5ff641085abc1aa58ed9f07523
Brian Check <b9check@gmail.com>
2025-01-05 01:48:35 -0500

    updated site

--- personalsite/main/urls.py
+++ personalsite/main/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('projects/', views.projects, name='projects'),
     path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('resume/', views.resume, name='resume')
 
 ]
